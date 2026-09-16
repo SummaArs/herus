65d642f504bb5cf5fd23c33333a2fd923c7656ae
SummaArs <gustavo.goncalves@imcorptec.com.br>
2026-09-16 10:36:34 -0300

    research: expose frontier primitives

--- research/symbiont_v2/__init__.py
+++ research/symbiont_v2/__init__.py
@@ -1,22 +1,20 @@
 """HERUS Symbiont v2 experimental research package."""
 
 from .core import (
-    AbstractSkill,
-    DiscoveryBudget,
-    Effect,
-    Goal,
-    HostAdapter,
-    HostContext,
-    HostModel,
-    Observation,
-    PersistentMemory,
-    PrimitiveAction,
-    State,
+    AbstractSkill, DiscoveryBudget, Effect, Goal, HostAdapter, HostContext,
+    HostModel, Observation, PersistentMemory, PrimitiveAction, State,
     SymbiontRuntime,
+)
+from .frontier import (
+    ActiveProbePlanner, CapabilityGraph, CapabilityNode, InformationGainModel,
+    Interval, MemoryItem, ModularMemory, ProbeCandidate, ScalarBelief,
+    SemanticEffect,
 )
 
 __all__ = [
-    "AbstractSkill", "DiscoveryBudget", "Effect", "Goal", "HostAdapter",
-    "HostContext", "HostModel", "Observation", "PersistentMemory",
-    "PrimitiveAction", "State", "SymbiontRuntime",
+    "AbstractSkill", "ActiveProbePlanner", "CapabilityGraph", "CapabilityNode",
+    "DiscoveryBudget", "Effect", "Goal", "HostAdapter", "HostContext",
+    "HostModel", "InformationGainModel", "Interval", "MemoryItem",
+    "ModularMemory", "Observation", "PersistentMemory", "PrimitiveAction",
+    "ProbeCandidate", "ScalarBelief", "SemanticEffect", "State", "SymbiontRuntime",
 ]
