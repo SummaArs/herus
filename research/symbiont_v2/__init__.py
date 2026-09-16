dc97b8b86560db509fbb93a491b3b7d2d2884e4b
SummaArs <gustavo.goncalves@imcorptec.com.br>
2026-09-16 10:33:58 -0300

    research: expose symbiont v2 package

---
+++ research/symbiont_v2/__init__.py
@@ -0,0 +1,22 @@
+"""HERUS Symbiont v2 experimental research package."""
+
+from .core import (
+    AbstractSkill,
+    DiscoveryBudget,
+    Effect,
+    Goal,
+    HostAdapter,
+    HostContext,
+    HostModel,
+    Observation,
+    PersistentMemory,
+    PrimitiveAction,
+    State,
+    SymbiontRuntime,
+)
+
+__all__ = [
+    "AbstractSkill", "DiscoveryBudget", "Effect", "Goal", "HostAdapter",
+    "HostContext", "HostModel", "Observation", "PersistentMemory",
+    "PrimitiveAction", "State", "SymbiontRuntime",
+]
